7cba0a8f7926e19983b0df95332ce6503895118e
nmaha <nmaha@gmail.com>
2023-05-10 17:52:06 +0530

    table

--- src/Table.svelte
+++ src/Table.svelte
@@ -1,43 +1,30 @@
 <script>
-    let data = [
-      { id: 1, name: 'Janu', age: 35, mobileno:9874455332 },
-      { id: 2, name: 'maha', age: 22, mobileno:8934235894},
-      { id: 3, name: 'kavya', age: 42, mobileno:7676756767}
+    let rows = [
+      { id: 1, name: "maha", email: "maha@example.com" },
+      { id: 2, name: "kavya", email: "kavya@example.com" }
     ];
     
+    let newId = rows.length + 1;
+    let newRow = { id: newId, name: "", email: "" };
+    let editingRow = null;
     
-    function deleteRow(id) {
-      data = data.filter(item => item.id !== id);
+    function removeRow(id) {
+      rows = rows.filter(row => row.id !== id);
     }
-    function createRow(newItem) {
-      data = [...data, newItem];
-    }
-  
-
-    function updateRow(id, newData) {
-      data = data.map(item => item.id === id ? { ...item, ...newData } : item);
-    }
-    function create(event) {
-      const formData = new FormData(event.target);
-      const newItem = {
-        id: data.length + 1,
-        name: formData.get('name'),
-        age: formData.get('age'),
-        mobileno: formData.get('mobileno')
-      };
-      createRow(newItem);
-      event.target.reset();
-    }
-  
-    function update(event, id) {
-      const formData = new FormData(event.target);
-      const newData = {
     
-        name: formData.get('name'),
-        age: formData.get('age'),
-        mobileno:formData.get('mobileno')
-      };
-      updateRow(id, newData);
+    function addRow() {
+      rows = [...rows, newRow];
+      editingRow = newRow;
+      newId++;
+      newRow = { id: newId, name: "", email: "" };
+    }
+    
+    function saveRow(row) {
+      let index = rows.findIndex(r => r.id === row.id);
+      if (index !== -1) {
+        rows[index] = row;
+        editingRow = null;
+      }
     }
   </script>
   
@@ -46,35 +33,40 @@
       <tr>
         <th>ID</th>
         <th>Name</th>
-        <th>Age</th>
-        <th>mobileno</th>
-        <th>Action</th>
+        <th>Email</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-      {#each data as item}
-        <tr>
-          <td>{item.id}</td>
-          <td>{item.name}</td>
-          <td>{item.age}</td>
-          <td>{item.mobileno}</td>
-          <td><button on:click={() => deleteRow(item.id)}>Delete</button></td>
-        </tr>
+      {#each rows as row}
+        {#if editingRow === row}
+          <tr>
+            <td>{row.id}</td>
+            <td><input bind:value={row.name} /></td>
+            <td><input bind:value={row.email} /></td>
+            <td>
+              <button on:click={() => saveRow(row)}>Save</button>
+              <button on:click={() => editingRow = null}>Cancel</button>
+            </td>
+          </tr>
+        {:else}
+          <tr>
+            <td>{row.id}</td>
+            <td>{row.name}</td>
+            <td>{row.email}</td>
+            <td>
+              <button on:click={() => editingRow = row}>Edit</button>
+              <button on:click={() => removeRow(row.id)}>Delete</button>
+            </td>
+          </tr>
+        {/if}
       {/each}
-    </tbody>
-
-    <tfoot>
       <tr>
-        <td colspan="4">
-          <form on:submit|preventDefault={create}>
-            <input name="name" placeholder="Name" required>
-            <input type="number" name="age" placeholder="Age" required>
-            <input type="number" name="mobileno" placeholder="mobileno" required>
-            <button type="submit">Create</button>
-          </form>
-        </td>
+        <td>{newId}</td>
+        <td><input bind:value={newRow.name} /></td>
+        <td><input bind:value={newRow.email} /></td>
+        <td><button on:click={addRow}>Add</button></td>
       </tr>
-    </tfoot>
+    </tbody>
   </table>
-  
   
