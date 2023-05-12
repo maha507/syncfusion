05de107d6dec9bbbec3f560f56498808e10bf25a
nmaha <nmaha@gmail.com>
2023-05-12 17:29:55 +0530

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,48 @@
-<h1>hiiiiiiiiiii</h1>
+<script>
+  import Grid from "gridjs-svelte"
+
+
+  const data = [
+    {
+      State: 'California',
+      Capital: 'Sacramento',
+      Language: 'English',
+      Population:''
+    },
+    {
+      State: 'Texas',
+      Capital: 'Austin',
+      Language: 'English',
+      Population: ''
+    },
+    {
+      State: 'New York',
+      Capital: 'Albany',
+      Language: 'English',
+      Population: ''
+    },
+    {
+      State: 'Hawaii',
+      Capital: 'Honolulu',
+      Language: 'English, Hawaiian',
+      Population: ''
+    },
+    {
+      State: 'Louisiana',
+      Capital: 'Baton Rouge',
+      Language: 'English',
+      Population: ''
+    }
+  ]
+</script>
+
+<Grid data={data}
+sort
+search
+pagination={{ enabled: true, limit: 3 }}
+ 
+/>
+
+<style global>
+  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
+</style>
