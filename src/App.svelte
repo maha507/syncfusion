98db124ca449576bb0c9263e54df998e531b9dad
nmaha <nmaha@gmail.com>
2023-05-12 18:23:16 +0530

    ADD

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,26 @@
 <script>
   import Grid from "gridjs-svelte"
 
-
   const data = [
-    {
-      State: 'California',
-      Capital: 'Sacramento',
-      Language: 'English',
-      Population:''
-    },
-    {
-      State: 'Texas',
-      Capital: 'Austin',
-      Language: 'English',
-      Population: ''
-    },
-    {
-      State: 'New York',
-      Capital: 'Albany',
-      Language: 'English',
-      Population: ''
-    },
-    {
-      State: 'Hawaii',
-      Capital: 'Honolulu',
-      Language: 'English, Hawaiian',
-      Population: ''
-    },
-    {
-      State: 'Louisiana',
-      Capital: 'Baton Rouge',
-      Language: 'English',
-      Population: ''
-    }
+      { name: "maha", email: "maha@example.com", age: "23", mobileno: "9873366463" },
+      { name: "Manasa", email: "manasa@gmail.com",  age: "21",mobileno: "9876545678"},
+      {name: "giri", email: "giri@gmail.com", age:"18" , mobileno: "8766554324"},
+      { name: "mahesh", email: "mahesh@example.com", age: "22", mobileno: "6873366463" },
+      { name: "kavya", email: "kavya@gmail.com",  age: "21",mobileno: "9876545678"},
+      {name: "ganesh", email: "ganesh@gmail.com", age:"19" , mobileno: "7766554324"},
+      { name: "madhu", email: "madhu@example.com", age: "24", mobileno: "7873366463" },
+      { name: "vicky", email: "vicky@gmail.com",  age: "21",mobileno: "8876545678"},
+      {name: "gagan", email: "gagan@gmail.com", age:"19" , mobileno: "9766554324"}
   ]
 </script>
 
-<Grid data={data}
+<Grid 
 sort
-search
-pagination={{ enabled: true, limit: 3 }}
- 
-/>
+search 
+pagination={{ enabled: true, limit: 5 }}
+data={data}/>
 
 <style global>
   @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
 </style>
+  
