fcb47f33c3298996082bfd8534ce82e205042556
nmaha <nmaha@gmail.com>
2023-05-12 09:40:31 +0530

    7 commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,106 @@
 <script>
-  let pendingItems = [
-    { id: 1, name: 'Task 1', description: 'complete inroduction part'},
-    { id: 2, name: 'Task 2', description: 'have to learn Basic topics in the tool',  },
+  let applicants = [
+    { names: ['task 1', 'task 2', 'task 3'] },
+    { names: ['task 4', 'task 5'] },
+    { names: ['Task 6', 'Task 7', 'Task 8'] }
   ];
-  let completedItems=[];
-  function addItem() {
-    const name = prompt('Enter the name of the task');
-    const description = prompt('Enter the description of the task');
-    const id = pendingItems.length + 1;
-    pendingItems = [...pendingItems, { id, name, description, status: 'pending' }];
+
+  let hoveringOverApplicant = null;
+
+  function dragStart(event, applicantIndex, nameIndex) {
+    const data = { applicantIndex, nameIndex };
+    event.dataTransfer.setData('text/plain', JSON.stringify(data));
   }
 
-  function editItem(item) {
-    const name = prompt('Enter the new name of the task', item.name);
-    const description = prompt('Enter the new description of the task', item.description);
-    pendingItems = pendingItems.map(i => i.id === item.id ? { ...i, name, description } : i);
+  function drop(event, applicantIndex, nameIndex = null) {
+event.preventDefault();
+const json = event.dataTransfer.getData('text/plain');
+const data = JSON.parse(json);
+
+if (nameIndex !== null) {
+  // Reordering names within the same applicant
+  const [name] = applicants[data.applicantIndex].names.splice(data.nameIndex, 1);
+  applicants[applicantIndex].names.splice(nameIndex, 0, name);
+} else {
+  // Moving name to a different applicant
+  const [name] = applicants[data.applicantIndex].names.splice(data.nameIndex, 1);
+  applicants[applicantIndex].names.push(name);
+}
+
+applicants = [...applicants]; // Make a copy to trigger reactivity
+hoveringOverApplicant = null;
+}
+
+  function deleteName(applicantIndex, nameIndex) {
+    applicants[applicantIndex].names.splice(nameIndex, 1);
+    applicants = [...applicants]; // Make a copy to trigger reactivity
   }
 
-  function deleteItem(item) {
-    pendingItems = pendingItems.filter(i => i.id !== item.id);
-    completedItems = completedItems.filter(i => i.id !== item.id);
+  function editName(applicantIndex, nameIndex, newName) {
+    applicants[applicantIndex].names[nameIndex] = newName;
+    applicants = [...applicants]; // Make a copy to trigger reactivity
   }
 
-  function handleDrop(event) {
-    const itemId = event.dataTransfer.getData('itemId');
-    const item = pendingItems.find(i => i.id === Number(itemId));
-    pendingItems = pendingItems.filter(i => i.id !== Number(itemId));
-    completedItems = [...completedItems, { ...item, status: 'completed' }];
+  function addName(applicantIndex) {
+    const newName = prompt('Enter a new name:');
+    if (newName) {
+      applicants[applicantIndex].names.push(newName);
+      applicants = [...applicants]; // Make a copy to trigger reactivity
+    }
   }
 </script>
 
-<div class="container">
-  <div class="pending-items" on:drop={handleDrop}>
-    
-    {#each pendingItems as item}
-      <div class="item" draggable="true" on:dragstart={() => event.dataTransfer.setData('itemId', item.id)}>
-        <h3>{item.name}</h3>
-        <p>{item.description}</p>
-        <button on:click={() => editItem(item)}>Edit</button>
-        <button on:click={() => deleteItem(item)}>Delete</button>
-      </div>
-    {/each}
-  </div>
-  <div class="completed-items">
-    
-    {#each completedItems as item}
-      <div class="item">
-        <h3>{item.name}</h3>
-        <p>{item.description}</p>
-      </div>
-    {/each}
-  </div>
-  <h1></h1>
-  <button on:click={addItem}>Add task</button>
+<div class="applicants">
+  {#each applicants as applicant, i}
+    <div
+      class="applicant"
+      on:dragover={() => hoveringOverApplicant = i}
+      on:drop={() => drop(Event, i)}
+      on:dragleave={() => hoveringOverApplicant = null}
+      style="background-color: {i === hoveringOverApplicant ? 'lightgray' : 'white'};"
+    >
+      <h2>these Tasks are in  {i + 1} <button on:click={() => addName(i)}>Add Name</button></h2>
+      <ul>
+        {#each applicant.names as name, j}
+          <li
+            class="name"
+            draggable="true"
+            on:dragstart={() => dragStart(Event, i, j)}
+          >
+            <div style="display: flex; align-items: center;">
+              <span>{name}</span>
+              <div style="margin-left: 10px;">
+                <button on:click={() => deleteName(i, j)}>Delete</button>
+                <button on:click={() => editName(i, j, prompt('Enter a new name:'))}>Edit</button>
+              </div>
+            </div>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  {/each}
 </div>
+
+<style>
+  .applicants {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .applicant {
+    width: 400px;
+    padding: 20px;
+    border: 1px solid black;
+    margin: 10px;
+  }
+
+  .name {
+    cursor: move;
+    margin: 5px;
+    padding: 10px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
+
